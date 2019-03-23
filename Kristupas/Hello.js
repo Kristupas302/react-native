70d03f9d233c22d497379c9782f6a4b2aae5cbd9
Mokinys <Kristupas302>
2019-03-23 14:23:26 +0200

    pirma pamoka

---
+++ Kristupas/Hello.js
@@ -0,0 +1,16 @@
+
+
+function pridetiZodiLabas() {
+	var hello = document.getElementById("hello").textContent;
+	document.getElementById("hello").innerHTML = hello + " labas " ;
+}
+
+
+
+var mygtukas = document.getElementById("mygtukas").addEventListener("click", pridetiZodiLabas);
+function sam (skaicius1 , skaicius2) {
+(console.log ())
+}
+
+
+
---
+++ Kristupas/saveScript.js
@@ -0,0 +1,30 @@
+function daugintiMetuSuma (pirmasParametras, antrasParametras) {
+	var metuSandauga = pirmasParametras * antrasParametras;
+	console.log(metuSandauga);
+}
+daugintiMetuSuma(6, 2);
+daugintiMetuSuma(15, 8);
+daugintiMetuSuma(48,554);
+var pirmasSkaicius = 12;
+var antrasSkaicius = 120;
+function sudauginti () {
+	var sandauga = pirmasSkaicius * antrasSkaicius;
+	console.log (sandauga);
+}
+sudauginti(12,120);
+function atimti () {
+	var atimtis = pirmasSkaicius - antrasSkaicius;
+	console.log (atimtis);
+}
+atimti(120,12);
+function padalinti (pirmasSkaitmuo,antrasSkaitmuo) {
+	var dalyba = pirmasSkaitmuo / antrasSkaitmuo;
+    console.log (dalyba);
+}
+padalinti(222222222,6);
+
+function sudeti (vienas,du) {
+    var sudetis = vienas + du
+    console.log(sudetis);
+}
+sudeti(5555,20000);
